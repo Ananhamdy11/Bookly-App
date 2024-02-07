1e5b2b528918e6a71e7756138d6fe7db4aeea0ec
Anan Hamdy <nonahamdy205@gmail.com>
2024-02-07 22:29:27 +0200

    create book details view

---
+++ lib/feauters/home/presentation/views/book_detail_view.dart
@@ -0,0 +1,14 @@
+import 'package:bookly/feauters/home/presentation/views/widgets/book_detail_view_body.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class BookDetailView extends StatelessWidget {
+  const BookDetailView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body:BookDetailsViewBody(),
+    );
+  }
+}
---
+++ lib/feauters/home/presentation/views/widgets/book_detail_view_body.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+class BookDetailsViewBody extends StatelessWidget {
+  const BookDetailsViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+      
+    );
+  }
+}
