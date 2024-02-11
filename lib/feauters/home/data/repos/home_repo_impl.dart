70534b75820607c8af43051d5a19cd2356558893
Anan Hamdy <nonahamdy205@gmail.com>
2024-02-12 00:19:30 +0200

    implement fetch feauterd books method

--- lib/feauters/home/data/repos/home_repo_impl.dart
+++ lib/feauters/home/data/repos/home_repo_impl.dart
@@ -33,9 +33,26 @@ return left(ServerFailure(e.toString()));
   }
 
   @override
-  Future<Either<Failures, List<BookModel>>> fetchFeautreBooks() {
-    // TODO: implement fetchFeautreBooks
-    throw UnimplementedError();
+  Future<Either<Failures, List<BookModel>>> fetchFeautreBooks() async{
+try {
+  var data= await apiService.get
+  (endPoint: 'volumes?Filtering=free-ebooks&q=subject:programming');
+
+List<BookModel> books=[];
+
+for (var item in data['items']) {
+  books.add(BookModel.fromJson(item));
+}
+return right(books);
+
+} on Exception catch (e) {
+if(e is DioError){
+  return left(ServerFailure.fromDioError(e));
+}
+return left(ServerFailure(e.toString()));
+
+}
+  
   }
 
 }
